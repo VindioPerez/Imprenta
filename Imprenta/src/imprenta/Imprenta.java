69921681f1da9b530980e2f59c463e5f86447c8c
Vindio Pérez <vpgdkrglc@gmail.com>
2019-11-25 19:07:11 +0100

    añadidos parse exceptions

--- Imprenta/src/imprenta/Imprenta.java
+++ Imprenta/src/imprenta/Imprenta.java
@@ -5,6 +5,8 @@
  */
 package imprenta;
 
+import java.text.ParseException;
+
 /**
  *
  * @author DAW108
@@ -14,8 +16,24 @@ public class Imprenta {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // TODO code application logic here
+        Cliente a = Cliente.nuevoCliente();
+        Departamento b = Departamento.nuevoDepartamento();
+        Labor c = Labor.nuevoLabor();
+        Libro d = Libro.nuevoLibro();
+        Lote e = Lote.nuevoLote();
+        Maquina f = Maquina.nuevoMaquina();
+        Modificacion g = Modificacion.nuevaModificacion();
+        OCalidad h = OCalidad.nuevoOCalidad();
+        OImpresion i = OImpresion.nuevoOImpresion();
+        OMaquinas j = OMaquinas.nuevoOMaquinas();
+        Operario k = Operario.nuevoOperario();
+        Politica l = Politica.nuevaPolitica();
+        Poster m = Poster.nuevoPoster();
+        Rotulo n = Rotulo.nuevoRotulo();
+        Trabajo o = Trabajo.nuevoTrabajo();
+        
     }
 
 }
