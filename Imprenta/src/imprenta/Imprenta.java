d3e02197a36f3726bd9806d2c6ab3096ed715a61
Vindio Pérez <vpgdkrglc@gmail.com>
2020-01-22 18:50:26 +0100

    Progresos en metodo main

--- Imprenta/src/imprenta/Imprenta.java
+++ Imprenta/src/imprenta/Imprenta.java
@@ -6,6 +6,8 @@
 package imprenta;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -13,27 +15,59 @@ import java.text.ParseException;
  * @version 1.1
  */
 public class Imprenta {
-
+    public static ArrayList<Cliente> clientes = new ArrayList<>();
+    public static ArrayList<Trabajo> trabajos = new ArrayList<>();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ParseException {
         // TODO code application logic here
-        Cliente a = Cliente.nuevoCliente();
-        Departamento b = Departamento.nuevoDepartamento();
-        Labor c = Labor.nuevoLabor();
-        Libro d = Libro.nuevoLibro();
-        Lote e = Lote.nuevoLote();
-        Maquina f = Maquina.nuevoMaquina();
-        Modificacion g = Modificacion.nuevaModificacion();
-        OCalidad h = OCalidad.nuevoOCalidad();
-        OImpresion i = OImpresion.nuevoOImpresion();
-        OMaquinas j = OMaquinas.nuevoOMaquinas();
-        Operario k = Operario.nuevoOperario();
-        Politica l = Politica.nuevaPolitica();
-        Poster m = Poster.nuevoPoster();
-        Rotulo n = Rotulo.nuevoRotulo();
-        Trabajo o = Trabajo.nuevoTrabajo();
+        while(true){
+            System.out.println("¿Qué tipo de usuario eres?");
+            System.out.println("Si eres un cliente, introduce una C");
+            System.out.println("Si eres un operario de impresión, introduce una I");
+            System.out.println("Si eres un operario de calidad, introduce una Q");
+            System.out.println("Si eres un operario de maquinas, introduce una M");
+            System.out.println("Si quieres salir, introduce una S");
+            Scanner in = new Scanner(System.in);
+            char opcion = in.next().charAt(0);
+            switch (opcion){
+                case 'c':
+                case 'C':
+                    System.out.println("¿Qué quiere hacer?");
+                    System.out.println("Si quiere registrarse, pulse R");
+                    System.out.println("Si quiere encargar un trabajo, pulse E");
+                    System.out.println("Si quiere salir, pulse S");
+                    char opcionC = in.next().charAt(0);
+                    switch (opcionC){
+                        case 'r':
+                        case 'R':
+                            Cliente c = Cliente.registrarCliente();
+                            clientes.add(c);
+                            break;
+                        case 'e':
+                        case 'E':
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 'i':
+                case 'I':
+                    break;
+                case 'q':
+                case 'Q':
+                    break;
+                case 'm':
+                case 'M':
+                    break;
+                default:
+                    break;
+            }
+            if (opcion=='s'||opcion=='S') break;
+        
+        
+        }
         
     }
 
