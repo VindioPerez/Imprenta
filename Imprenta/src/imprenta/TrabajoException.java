b5e6ce75d7ee092363b604f3386c4b6d20be5dfe
RocknRolla42 <deran115@gmail.com>
2020-01-27 17:29:58 +0100

    TrabajoException añadido

---
+++ Imprenta/src/imprenta/TrabajoException.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imprenta;
+
+/**
+ *
+ * @author vindi
+ */
+public class TrabajoException extends RuntimeException{
+
+    public TrabajoException() {
+    }
+
+    public TrabajoException(String message) {
+        super(message);
+    }
+    
+    
+    
+}
