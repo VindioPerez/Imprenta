d7ad50afeca8a1561c6811754ba735e0a3e5dca7
Alberto <Alberto@Alberto-PC>
2020-01-27 22:32:54 +0100

    incorporación metodo realizarLabor

---
+++ Imprenta/src/imprenta/LaborException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imprenta;
+
+/**
+ *
+ * @author Alberto
+ */
+public class LaborException extends RuntimeException{
+    
+     public LaborException() {
+    }
+
+    public LaborException(String message) {
+        super(message);
+    }
+}
