c124c67137f9e10699a09bacdc710383cd324a07
AlbertoLinan <56266283+AlbertoLinan@users.noreply.github.com>
2020-02-19 20:13:13 +0100

    Cambios en Labor, LaborException, implementacion de metodos de escritura y lectura

--- Imprenta/src/imprenta/LaborException.java
+++ Imprenta/src/imprenta/LaborException.java
@@ -5,6 +5,9 @@
  */
 package imprenta;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author Alberto
@@ -16,5 +19,25 @@ public class LaborException extends RuntimeException{
 
     public LaborException(String message) {
         super(message);
+    }
+    
+    
+    public static boolean comprobarFechaini(Date fechaini){
+          return fechaini!=null;
+    }
+    public static boolean comprobarFechafin(Date fechafin){
+        return fechafin!=null;
+    }
+    public static boolean comprobarTarea(String tarea){
+    return !tarea.isEmpty();
+    }
+    public static boolean comprobarMaquina(Maquina maquina){
+        return maquina!=null;
+    }
+    public static boolean comprobarOperariosM(ArrayList<OMaquinas> operariosM){
+      return operariosM !=null;  
+    }
+    public static boolean comprobarIdMaquina (long idMaquina) {
+        return idMaquina>0;
     }
 }
